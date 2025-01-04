77ae450e320ce6d3cdbf231b4c55d5d3af049a69
Lukasz <lukieski@gmail.com>
2025-01-04 21:08:35 +0200

    Working version for printing recipes

--- print/printHandler.js
+++ print/printHandler.js
@@ -1,8 +1,15 @@
 import ejs from 'ejs';
-import pdf from 'html-pdf-node';
+import dotenv from 'dotenv';
+import puppeteer from "puppeteer-core";
+import fs from 'fs';
 
 import { getGlobalID } from '../util/util.js'
 import { queryMetaObject } from '../shopify/query.js'
+
+
+
+// Load environment variables
+dotenv.config();
 
 export async function printCustomerRecipe(req, res) {
 
@@ -13,30 +20,44 @@ export async function printCustomerRecipe(req, res) {
     const MetaObject = await queryMetaObject(recipeGID);
     console.log(MetaObject);
 
+    
+
     console.log(`Printing recipe ID '${recipeID} for customer ID '${customerID}`);
 
-    const outputName = `${MetaObject.title.replace(/\s+/g, '_')}.pdf`;
-    const pdfBuffer = await generatePdf(MetaObject);
+    const outputName = `${MetaObject.title}`.replaceAll(' ', '_');
+    const outputFile = `${outputName}.pdf`;
+    const outputDir = `./print/output`;
+    const outputPdfPath = `./print/output/${outputFile}`;
+
+    const pdfBuffer = await generatePdf(MetaObject, outputDir, outputName);
 
     // Set the response headers
-    res.setHeader('Content-Type', 'application/pdf');
     res.setHeader(
+        'Content-Type', 'application/pdf',
         'Content-Disposition',
-        `attachment; filename="${outputName}"`
+        `attachment; filename="${outputFile}"`
     );
     res.send(pdfBuffer);
 
 }
 
-async function generatePdf(recipeData) {
+async function generatePdf(recipeData, outputDir, fileName) {
     try {
         // Render the template with dynamic data
         const html = await ejs.renderFile('./print/templateRecipe.ejs', { recipeData });
-        const pdfBuffer = await pdf.generatePdf({ content: html }, { format: 'A4' });
-        
+        const browser = await puppeteer.connect({ browserWSEndpoint: process.env.BROWSER_WS_ENDPOINT });
+        const page = await browser.newPage();
+
+        // Set the HTML content
+        await page.setContent(html, { waitUntil: 'load' });
+        fs.writeFileSync(`${outputDir}/debug_${fileName}.html`, html);
+        // const pdfBuffer = await page.pdf({  path: `${outputDir}/${fileName}.pdf`, format: 'A4' });
+        const pdfBuffer = await page.pdf({ format: 'A4' });
+        fs.writeFileSync(`${outputDir}/debug_${fileName}.pdf`, pdfBuffer);
+
         console.log(`PDF for ${recipeData.title} generated successfully!`);
 
-        return pdfBuffer;
+        return Buffer.from(pdfBuffer);
     } catch (error) {
         console.error('Error generating PDF:', error);
     }
